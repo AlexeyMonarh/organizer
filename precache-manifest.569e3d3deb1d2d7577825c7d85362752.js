a46f927db52e11191c7478864d4494e9e7dd3349
AlexeyMonarh <monarhweb@gmail.com>
2021-02-11 01:23:40 +0300

    Updates

---
+++ precache-manifest.569e3d3deb1d2d7577825c7d85362752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d7448e761fdc786b301",
+    "url": "/organizer/static/css/main.94820d20.chunk.css"
+  },
+  {
+    "revision": "9d7448e761fdc786b301",
+    "url": "/organizer/static/js/main.478511bf.chunk.js"
+  },
+  {
+    "revision": "04fcbd9bd559dd13348c",
+    "url": "/organizer/static/js/runtime~main.110da72c.js"
+  },
+  {
+    "revision": "9d101f98ec9499757f67",
+    "url": "/organizer/static/css/2.7b4da414.chunk.css"
+  },
+  {
+    "revision": "9d101f98ec9499757f67",
+    "url": "/organizer/static/js/2.044de0e1.chunk.js"
+  },
+  {
+    "revision": "d799e224dcd68188d7392a00a5542c56",
+    "url": "/organizer/index.html"
+  }
+];
